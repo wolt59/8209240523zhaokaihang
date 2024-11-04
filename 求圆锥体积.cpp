c5547239bc5cc2a8ad6caad15b3752e1a96bec00
wolt59 <824850843@qq.com>
2024-11-04 22:13:34 +0800

    Add files via upload

---
+++ 求圆锥体积.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int r, h;
+	const float PI = 3.14;
+	cout << "请输入圆锥半径 "<< endl;
+	cin >>r ;
+	cout << "请输入圆锥的高" << endl;
+	cin >>h ;
+	cout << "圆锥的体积为" <<  PI * r * r * h/3;
+	return 0;
+}
